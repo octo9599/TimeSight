2ce938c4bd2f2ac71f7a4549da0c57bd5fa32841
ImSacki11 <1044@htl.rennweg.at>
2025-12-02 13:14:23 +0100

    listview.vue import

--- TimeSightVue/src/App.vue
+++ TimeSightVue/src/App.vue
@@ -2,11 +2,13 @@
 import SideBar from "@/components/SideBar.vue";
 import TopBar from "@/components/TopBar.vue";
 //import apiTest from "./apiTests/apiTest.vue";
+//import ListView from "./components/ListView.vue";
 </script>
 
 <template>
   <TopBar/>
   <SideBar/>
+  <!-- <ListView/> -->
   <!-- <apiTest/> -->
 </template>
 
