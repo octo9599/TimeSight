4837f6cb0b681a16a913ea4bdb6471bdd7b77414
octo9599 <felix.icloud.at@icloud.com>
2025-12-08 02:21:39 +0100

    Update App.vue: changed ./ to @/ (meaning src/), also...
    new styles, fixed formatting,

--- TimeSightVue/src/App.vue
+++ TimeSightVue/src/App.vue
@@ -1,30 +1,39 @@
 <script setup>
 import SideBar from "@/components/SideBar.vue";
 import TopBar from "@/components/TopBar.vue";
-//import apiTest from "./apiTests/apiTest.vue";
-import ListView from "./components/ListView.vue";
+//import apiTest from "@/apiTests/apiTest.vue";
+import ListView from "@/components/ListView.vue";
 </script>
 
 <template>
-  <TopBar/>
-  <div id="horizontal-container">
-    <SideBar/>
-  </div>
-  <!-- <ListView/> -->
-  <ListView/>
-  <!-- <apiTest/> -->
+	<TopBar/>
+	<div id="horizontal-container">
+		<SideBar/>
+		<!-- #test is just for showing the correct bg-color currently, should be removed when work on calendar view begins   -->
+		<p id="test">Hello World this is a paragraph</p>
+	</div>
+	<!-- <ListView/> -->
+	<ListView/>
+	<!-- <apiTest/> -->
 </template>
 
 <style scoped>
 /* make font available globally */
 @font-face {
-  font-family: JockeyOne;
-  src: url('@/assets/fonts/JockeyOne-Regular.ttf');
+	font-family: JockeyOne;
+	src: url('@/assets/fonts/JockeyOne-Regular.ttf');
 }
 
 #horizontal-container {
-  display: flex;
-  flex-direction: row;
+	display: flex;
+	flex-direction: row;
+	height: 88vh;
+}
 
+#test {
+	width: 100%;
+	background-color: var(--main-dark);
+	margin: 0;
+	color: var(--text-dark);
 }
 </style>
