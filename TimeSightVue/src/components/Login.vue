0c43c7280b4754f2b164ea94970e203405890941
TvTBread <trish@pop-os.fritz.box>
2025-12-12 16:18:42 +0100

    Update Login.vue: implement the base Login form and backend-call.

--- TimeSightVue/src/components/Login.vue
+++ TimeSightVue/src/components/Login.vue
@@ -1,6 +1,30 @@
-<script setup lang="ts">
+<script setup>
 
-    
+    import { ref } from 'vue';
+    import axios from 'axios';
+
+    const API = "http://localhost:3000"
+    const email = ref("");
+    const password = ref("");
+
+    async function loginSubmit() {
+
+        console.log(email.value);
+        console.log(password.value);
+
+        let user;
+
+        try {
+
+            const res = await axios.post(`${API}/login`, { email: email.value, passwort: password.value }, { withCredentials: true });
+
+            console.log(res.data);
+            return res.data;
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
 </script>
 
@@ -8,6 +32,12 @@
     <p>
         Login
     </p>
+
+    <form @submit.prevent="loginSubmit">
+        <input type="email" placeholder="E-Mail" v-model="email" required /> <br>
+        <input type="password" placeholder="Password" v-model="password" required /> <br>
+        <button type="submit">login</button>
+    </form>
 </template>
 
 <style scoped>
