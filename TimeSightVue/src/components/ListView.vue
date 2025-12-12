046c4c8fa54d606e1b919f49b75c776c287a7b4c
ImSacki11 <neviomader1306@gmail.com>
2025-12-12 22:57:36 +0100

    add imports, base liststructure and basic css

--- TimeSightVue/src/components/ListView.vue
+++ TimeSightVue/src/components/ListView.vue
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = "http://localhost:3000";
 
 import { ref, onMounted, toRaw } from "vue";
-import { fetchTermine } from "./DataAccess.js";
+import { fetchData, formatDate } from "./DataAccess.js";
 
 const toDoTermine = ref([]);
 const overTermine = ref([]);
@@ -11,7 +11,7 @@ const doneTermine = ref([]);
 const dates = ref(new Set());
 
 onMounted(async () => {
-    const data = await fetchTermine();
+    const data = await fetchData();
     toDoTermine.value = data.toDoTermine;
     overTermine.value = data.overTermine;
     doneTermine.value = data.doneTermine;
@@ -22,7 +22,22 @@ onMounted(async () => {
 </script>
 
 <template>
-    <h1>hello</h1>
+    <dl>
+        <template v-for="date in dates">
+            <dt><h3>{{ formatDate(date) }}</h3></dt>
+            <dd> example </dd>
+        </template>
+    </dl>
 </template>
 
-<style></style>
+<style scoped>
+dt {
+    margin-left: 1%;
+}
+dd {
+    margin: 1% 20% 1% 3%;
+    padding: 1%;
+    border-radius: 1em;
+    background-color: var(--field-dark);
+}
+</style>
