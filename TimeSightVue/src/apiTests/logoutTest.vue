7af8f5412af459d26414eacdf93fbde79b9646e7
TvTBread <trish@pop-os.fritz.box>
2025-12-13 17:44:23 +0100

    Create TimeSightVue/src/apiTests/logoutTest.vue: implemented a test logout button.

---
+++ TimeSightVue/src/apiTests/logoutTest.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from 'axios'
+
+const API = "http://localhost:3000";
+
+    async function logout() {
+
+        try {
+            await axios.post(`${API}/logout`);
+            window.location.reload();
+        } catch(err) {
+            console.log(err);
+        }
+
+    }
+</script>
+
+<template>
+  <button @click="logout"> Logout </button>
+</template>
+
+<style scoped>
+
+</style>
