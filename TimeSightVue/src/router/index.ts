3e79e6b98ab8e62217e77b0d9ba3b272a5321e4a
ImSacki11 <1044@htl.rennweg.at>
2025-12-16 13:34:57 +0100

    Update index.ts: add router.beforeEach (does not work yet, zwischenstand itpü)

--- TimeSightVue/src/router/index.ts
+++ TimeSightVue/src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory, type Router} from 'vue-router'
 import ListPage from '@/ListPage.vue';
 import AuthPage from '@/AuthPage.vue';
+import { getUserStore, getUserToken, user} from '@/components/DataAccess.mjs'
 
 const router: Router = createRouter({
   // would be used if the root of the app is not stored at /
@@ -11,5 +12,17 @@ const router: Router = createRouter({
     {path: "/auth", component: AuthPage}
   ],
 })
+
+router.beforeEach(async (to) => {
+  await getUserStore()
+  await getUserToken()
+
+  if (!user.value && to.path !== '/auth') {
+    return '/auth'
+  } else {
+    return '/'
+  }
+})
+
 
 export default router
