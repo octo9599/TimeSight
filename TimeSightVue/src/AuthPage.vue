6b8d9d1949a5f73999c3bee70656a703d988484e
tvtbread <tris.tha035@gmail.com>
2025-12-16 10:19:27 +0100

    Update Authpage.vue: remove local fetchUser code

--- TimeSightVue/src/AuthPage.vue
+++ TimeSightVue/src/AuthPage.vue
@@ -2,29 +2,11 @@
 
     import axios from 'axios';
     import { ref, onMounted } from 'vue';
-    import { useUserStore } from './stores/user';
-    import { useRouter } from 'vue-router';
-
     import Login from './components/Login.vue';
     import Signup from './components/Signup.vue';
 
     const isLogin = ref(true);
 
-    const router = useRouter();
-	const userStore = useUserStore();
-
-	onMounted( () => {
-		authorize_token();
-	});
-
-	async function authorize_token() {
-
-		await userStore.fetchUser();
-		if(userStore.user != null) {
-			router.push("/");
-		}
-
-	}
 
 </script>
 
