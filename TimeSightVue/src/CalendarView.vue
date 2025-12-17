883dcf4f9b2f6cd85515378979c5f00b5bce1a44
TvTBread <trish@pop-os.fritz.box>
2025-12-17 14:13:04 +0100

    Update CalendarView.vue: add buttons to switch between month and week view and change calendar-height to 100%

--- TimeSightVue/src/CalendarView.vue
+++ TimeSightVue/src/CalendarView.vue
@@ -1,14 +1,26 @@
 <script setup>
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 const calendarOptions = {
-  plugins: [dayGridPlugin],
+  plugins: [dayGridPlugin, timeGridPlugin],
+
   initialView: 'dayGridMonth',
-  events: 'http://localhost:3000/termine'
+  events: 'http://localhost:3000/termine',
+  firstDay: 1,
+  height: '100%',
+  headerToolbar: {
+    left: 'title prev,next today',
+    right: 'dayGridMonth,dayGridWeek'
+  },
+  buttonText: {
+    dayGridMonth: 'M',
+    dayGridWeek: 'W'
+  },
 }
 </script>
 
 <template>
-  <FullCalendar :options="calendarOptions" />
+    <FullCalendar :options="calendarOptions" />
 </template>
