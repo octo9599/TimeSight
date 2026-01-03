187528a6d75d5109fc30a88170b7fd9ca3d37552
tvtbread <tris.tha035@gmail.com>
2026-01-03 20:21:42 +0100

    Update AddTask.vue: implement base functionality for AddTask

--- TimeSightVue/src/components/AddTask.vue
+++ TimeSightVue/src/components/AddTask.vue
@@ -1,9 +1,54 @@
 <script setup>
+    import { onMounted, ref } from 'vue';
+    import { fetchData } from './DataAccess.mjs';
+    import axios from 'axios';
 
+    const API = "http://localhost:3000";
+
+    let data = null;
+    const groupsOfUser = ref("loading...");
+
+    const bezeichnung = ref("");
+    const beschreibung = ref("");
+    const datum = ref("");
+    const uhrzeit = ref("");
+    const groupSelect = ref(null);
+
+    onMounted(async () => {
+        data = await fetchData();
+        groupsOfUser.value = data.groupsIn.data;
+    });
+    
+    async function createTermin() {
+
+        console.log(datum.value);
+        console.log(uhrzeit.value);
+
+        await axios.post(`${API}/termin`, {
+            bezeichnung: bezeichnung.value,
+            beschreibung: beschreibung.value,
+            datum: datum.value,
+            uhrzeit: uhrzeit.value,
+            group_id: groupSelect.value,
+            user_id: data.user.pk_user_id
+        });
+
+    }
 </script>
 
 <template>
-
+    <form @submit.prevent="createTermin">
+        <h2>-- Termin Erstellen --</h2>
+        Bezeichnung: <input type="text" v-model="bezeichnung" placeholder="Familientreffen" required/> <br>
+        Beschreibung: <input type="text" v-model="beschreibung" placeholder="Treffen bei Oma"/> <br>
+        Datum: <input type="date" v-model="datum" placeholder="Batum" required/> <br>
+        Uhrzeit: <input type="time" v-model="uhrzeit" placeholder="Uhrzeit"/> <br>
+        für Gruppe: 
+        <select v-model="groupSelect" required>
+            <option v-for="group in groupsOfUser" :value="group.pk_group_id">{{group.gruppenname}}</option>
+        </select> <br>
+        <button type="submit">Erstellen</button>
+    </form>
 </template>
 
 <style scoped>
