193a0ebae338b8d0b4d26db751640418be74f47c
TvTBread <tris.tha035@gmail.com>
2026-01-06 14:48:14 +0100

    Update AccPopup.vue: implement basic buttons for popup menu

--- TimeSightVue/src/components/AccPopup.vue
+++ TimeSightVue/src/components/AccPopup.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-
+    <router-link to="/account">Account</router-link>
+    <button>Lightmode - Darkmode</button>
+    <router-link to="/logout">Log out</router-link>
 </template>
 
 <style scoped>
