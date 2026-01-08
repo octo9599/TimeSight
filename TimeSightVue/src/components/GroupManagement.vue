5e39fbe2e1482d44c90eb2e61d7f753ba6fce081
TvTBread <tris.tha035@gmail.com>
2026-01-08 21:05:56 +0100

    Update GroupManagement.vue: add group_id prop and close emit

--- TimeSightVue/src/components/GroupManagement.vue
+++ TimeSightVue/src/components/GroupManagement.vue
@@ -1,11 +1,14 @@
 <script setup>
 
+    const props = defineProps(['group_id'])
+    const emit = defineEmits(['close']);
+
 </script>
 
 <template>
-
+    
 </template>
 
 <style scoped>
-    
+
 </style>
