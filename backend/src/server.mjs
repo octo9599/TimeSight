90a9fd1e2053087781b8d6a217624e82abc586da
TvT <tris.tha035@gmail.com>
2026-01-19 13:00:40 +0100

    Update server.mjs: fix delete(/user) function

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -541,7 +541,7 @@ app.delete("/user/:user_id", async (req, res) => {
             return res.status(400).json({error: "user_id is required to delete a user"});
         }
 
-        await runQuery("DELETE tu FROM Termin_User WHERE fk_user_id = ?", [user_id]);
+        await runQuery("DELETE FROM Termin_User WHERE fk_user_id = ?", [user_id]);
         await runQuery("UPDATE Termin SET fk_ersteller_id = null WHERE fk_ersteller_id=?", [user_id]);
         await runQuery("DELETE FROM Gruppe_User WHERE fk_user_id = ?", [user_id]);
         await runQuery("DELETE FROM Beitritt_Anfrage WHERE fk_user_id = ?", [user_id]);
