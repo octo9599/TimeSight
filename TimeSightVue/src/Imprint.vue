430770bb50eabbc4e7317ec2f0e0a66dbcabb0f3
TvTBread <tris.tha035@gmail.com>
2026-01-19 20:27:15 +0100

    Update Imprint.vue: create imprint information

--- TimeSightVue/src/Imprint.vue
+++ TimeSightVue/src/Imprint.vue
@@ -3,9 +3,31 @@
 </script>
 
 <template>
+    <div class="imprint">
+        <p>
+            <h2>Information:</h2>
+            Diese Webseite ist ein Schulprojekt, welches rein für Bildungszwecke entwickelt wurde.
+        </p>
+        <p>
+            <h2>Webseiten-betreiber:</h2>
 
+            TimeSight Entwicklerteam - HTL Rennweg <br>
+            Rennweg 89b <br>
+            1030 Wien  <br>
+            Österreich
+        </p>
+        <p>
+            <h2>Kontakt:</h2>
+            <a id="mail" href="mailto:2060@htl.rennweg.at">2060@htl.rennweg.at</a>
+        </p>
+    </div>
 </template>
 
 <style scoped>
-    
+    .imprint {
+        margin: 1%;
+    }
+    #mail {
+        color: var(--accent-dark);
+    }
 </style>
